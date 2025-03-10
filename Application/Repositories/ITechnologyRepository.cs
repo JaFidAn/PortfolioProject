9f6861e6e7e8f1c7ba2276ab063c904c46063bdd
Jafidan <r.alagezov@gmail.com>
2025-03-10 14:14:17 +0400

    Technology üçün Repository əlavə edildi

---
+++ Application/Repositories/ITechnologyRepository.cs
@@ -0,0 +1,12 @@
+using Domain.Entities;
+
+namespace Application.Repositories;
+
+public interface ITechnologyRepository
+{
+    Task<Technology?> GetByIdAsync(Guid id);
+    Task<IEnumerable<Technology>> GetAllAsync();
+    Task<Guid> AddAsync(Technology technology);
+    Task UpdateAsync(Technology technology);
+    Task DeleteAsync(Guid id);
+}
---
+++ Persistence/Repositories/TechnologyRepository.cs
@@ -0,0 +1,43 @@
+using Application.Cqrs.Commands;
+using Application.Cqrs.Queries;
+using Application.Repositories;
+using Domain.Entities;
+
+namespace Persistence.Repositories;
+
+public class TechnologyRepository : ITechnologyRepository
+{
+    private readonly ITechnologyQuery _technologyQuery;
+    private readonly ITechnologyCommand _technologyCommand;
+
+    public TechnologyRepository(ITechnologyQuery technologyQuery, ITechnologyCommand technologyCommand)
+    {
+        _technologyQuery = technologyQuery;
+        _technologyCommand = technologyCommand;
+    }
+
+    public async Task<IEnumerable<Technology>> GetAllAsync()
+    {
+        return await _technologyQuery.GetAllAsync();
+    }
+
+    public async Task<Technology?> GetByIdAsync(Guid id)
+    {
+        return await _technologyQuery.GetByIdAsync(id);
+    }
+
+    public async Task<Guid> AddAsync(Technology technology)
+    {
+        return await _technologyCommand.CreateAsync(technology);
+    }
+
+    public async Task UpdateAsync(Technology technology)
+    {
+        await _technologyCommand.UpdateAsync(technology);
+    }
+
+    public async Task DeleteAsync(Guid id)
+    {
+        await _technologyCommand.DeleteAsync(id);
+    }
+}
